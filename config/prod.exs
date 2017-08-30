f06248980a91169c9304af716966d40a57165b2d
Max Neuvians <max@neuvians.net>
2017-08-30 15:05:55 -0400

    Fixes

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :idq_auth_demo, IdqAuthDemoWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "https://idq-auth-demo.herokuapp.com/", port: 443],
+  url: [scheme: "https", host: "idq-auth-demo.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
