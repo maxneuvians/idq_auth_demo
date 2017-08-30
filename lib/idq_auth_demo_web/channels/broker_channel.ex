2cb61bfc0dbf0588f99fca143148a992fbbad295
Max Neuvians <max@neuvians.net>
2017-08-30 14:49:39 -0400

    Updated to callbacks version

--- lib/idq_auth_demo_web/channels/broker_channel.ex
+++ lib/idq_auth_demo_web/channels/broker_channel.ex
@@ -13,28 +13,15 @@ defmodule IdqAuthDemoWeb.BrokerChannel do
 
   def handle_in("push", %{"title" => title, "body" => body}, socket) do
     username = socket.topic |> String.replace("broker:", "")
-    {:ok, _pid, id} = IdqAuth.Delegated.push(username, title, body)
-    check_push_result(0, id, username, 0)
-    {:reply, {:ok, %{id: id}}, socket}
+    IdqAuth.Delegated.push(username, title, body, &check_push_result/2, %{username: username})
+    {:reply, {:ok, %{}}, socket}
   end
 
-  defp check_push_result(0, id, username, attempt) do
-    Logger.info "Checking"
-    if attempt < 10 do
-      :timer.sleep(3_000)
-      check_push_result(IdqAuth.Delegated.Observer.result(id), id, username, attempt + 1)
-    else
-      IdqAuthDemoWeb.Endpoint.broadcast "broker:" <> username, "push_result", %{result: "Timed out"}
-    end
-  end
-
-  defp check_push_result(%{"description" => "accept"}, _id, username, _attempt) do
+  def check_push_result(%{"description" => "accept"}, %{username: username}) do
     IdqAuthDemoWeb.Endpoint.broadcast "broker:" <> username, "push_result", %{result: "Accepted"}
   end
 
-  defp check_push_result(%{"description" => "decline"}, _id, username, _attempt) do
+  def check_push_result(%{"description" => "decline"}, %{username: username}) do
     IdqAuthDemoWeb.Endpoint.broadcast "broker:" <> username, "push_result", %{result: "Declined"}
   end
-
-
 end
